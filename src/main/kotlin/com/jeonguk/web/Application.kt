0d8aab264f9468331a100c1054e1e2f0daeb84ff
Barry <kato1883@gmail.com>
2019-08-04 02:52:42 +0900

    - Configuration 수정 및 추가

--- src/main/kotlin/com/jeonguk/web/Application.kt
+++ src/main/kotlin/com/jeonguk/web/Application.kt
@@ -1,12 +1,20 @@
 package com.jeonguk.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["com.jeonguk.web"], exclude = [SecurityAutoConfiguration::class])
-class SpringbootKotlinJunit5Application
+@SpringBootApplication(
+        scanBasePackages = ["com.jeonguk.web"],
+        exclude = [
+            DataSourceAutoConfiguration::class,
+            DataSourceTransactionManagerAutoConfiguration::class,
+            HibernateJpaAutoConfiguration::class]
+)
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<SpringbootKotlinJunit5Application>(*args)
+    runApplication<Application>(*args)
 }
--- src/main/kotlin/com/jeonguk/web/JpaConfig.kt
+++ src/main/kotlin/com/jeonguk/web/JpaConfig.kt
@@ -1,9 +1,67 @@
 package com.jeonguk.web
 
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.Database
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import java.util.*
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
 
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = ["com.jeonguk.web.repository"])
-class JpaConfig {
+class JpaConfig(private val env: Environment) {
+
+    @Bean
+    fun entityManagerFactory(): LocalContainerEntityManagerFactoryBean {
+        val vendorAdapter = HibernateJpaVendorAdapter()
+        vendorAdapter.setDatabase(Database.H2)
+        vendorAdapter.setGenerateDdl(true)
+        val em = LocalContainerEntityManagerFactoryBean()
+        em.dataSource = dataSource()
+        em.setPackagesToScan("com.jeonguk.web.domain.entity")
+        em.jpaVendorAdapter = vendorAdapter
+        em.setJpaProperties(additionalProperties())
+        return em
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create().type(HikariDataSource::class.java).build()
+    }
+
+    @Bean
+    fun transactionManager(emf: EntityManagerFactory): PlatformTransactionManager {
+        val transactionManager = JpaTransactionManager()
+        transactionManager.entityManagerFactory = emf
+        return transactionManager
+    }
+
+    @Bean
+    fun exceptionTranslation(): PersistenceExceptionTranslationPostProcessor {
+        return PersistenceExceptionTranslationPostProcessor()
+    }
+
+    private fun additionalProperties(): Properties {
+        val properties = Properties()
+        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto"))
+        properties.setProperty("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"))
+        properties.setProperty("hibernate.current_session_context_class", env.getProperty("spring.jpa.properties.hibernate.current_session_context_class"))
+        properties.setProperty("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"))
+        properties.setProperty("hibernate.format_sql", env.getProperty("spring.jpa.properties.hibernate.format_sql"))
+        return properties
+    }
+
 }
